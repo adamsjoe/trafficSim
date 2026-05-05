85a62285e7c0eaf80bab4cf00992dfade00f03b0
Joseph Adams <joseph.adams@blueyonder.co.uk>
2026-05-05 09:24:09 +0100

    chore: debugging CORS

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/overpass',
+        destination: 'https://overpass-api.de/api/interpreter',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
